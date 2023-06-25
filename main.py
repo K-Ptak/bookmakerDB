c2da0570276c7e3d008cb5cfd688acb9613231a4
K-Ptak <kamil2001p@gmail.com>
2023-06-25 19:31:20 +0200

    Add privilege change to admin panel

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from tkinter import font
-
-import ttkbootstrap as tkk
+import ttkbootstrap as ttk
 from ttkbootstrap import Style, OUTLINE, INFO
-import tkinter as tk
-from view.betcard import BetCard
-from view.matchcard import MatchCard
+from model.betcard import BetCard
+from model.matchcard import MatchCard
 from view.registration import *
+from database.admin_privilege_handler import show_add_privilege_window, show_del_privilege_window
 
 # Tworzenie głównego okna aplikacji
-root = tkk.Window()
+root = ttk.Window()
 root.title("Zakłady")
 root.state('zoomed')
 root.geometry("1200x500+35+100")
@@ -39,8 +37,8 @@ for i in range(len(usernames)):
 # funkcja do zwracania okna z blędem
 def login_error():
     messagebox.showerror("Błąd logowania", "Nieprawidłowy login lub hasło.")
-    username_entry.delete(0, tkk.END)
-    password_entry.delete(0, tkk.END)
+    username_entry.delete(0, ttk.END)
+    password_entry.delete(0, ttk.END)
 
 
 # Funkcja do logowania
@@ -99,80 +97,76 @@ def show_login():
 
     clear_window()
 
-    app_label = tkk.Label(root, text="Aplikacja Bukmacherska", font=("Comic Sans MS", 36))
+    app_label = ttk.Label(root, text="Aplikacja Bukmacherska", font=("Comic Sans MS", 36))
     app_label.pack(pady=10)
 
-    login_label = tkk.Label(root, text="Zaloguj się", font=("Bahnschrift", 16))
+    login_label = ttk.Label(root, text="Zaloguj się", font=("Bahnschrift", 16))
     login_label.pack(pady=(120, 20))
 
-    username_label = tkk.Label(root, text="Login:")
+    username_label = ttk.Label(root, text="Login:")
     username_label.pack()
-    username_entry = tkk.Entry(root)
-    username_entry.pack()
+    username_entry = ttk.Entry(root)
+    username_entry.pack(pady=(0, 20))
 
-    password_label = tkk.Label(root, text="Hasło:")
+    password_label = ttk.Label(root, text="Hasło:")
     password_label.pack()
-    password_entry = tkk.Entry(root, show="*")
-    password_entry.pack()
+    password_entry = ttk.Entry(root, show="*")
+    password_entry.pack(pady=(0, 20))
 
-    login_button = tkk.Button(root, text="Zaloguj się", command=login)
+    login_button = ttk.Button(root, text="Zaloguj się", command=login)
     login_button.pack(pady=10)
 
     # Przycisk do otwierania okna rejestracji
-    register_button = tkk.Button(root, text="Zarejestruj się",  bootstyle=(INFO, OUTLINE), command=lambda: open_registration_window(root))
-    register_button.pack(side=tkk.RIGHT, padx=10, pady=10)
+    register_button = ttk.Button(root, text="Zarejestruj się",  bootstyle=(INFO, OUTLINE), command=lambda: open_registration_window(root))
+    register_button.pack(side=ttk.RIGHT, padx=10, pady=10)
 
 
 # Widok panelu administratora
 def show_admin_panel(username):
     clear_window()
 
-    admin_label = tkk.Label(root, text="Panel administratora", font=("Arial", 16))
+    admin_label = ttk.Label(root, text="Panel administratora", font=("Arial", 16))
     admin_label.pack(pady=10)
 
-    left_column = tkk.Frame(root)
-    left_column.pack(side=tkk.LEFT, padx=50)
+    left_column = ttk.Frame(root)
+    left_column.pack(side=ttk.LEFT, padx=50)
 
-    login_value_label = tkk.Label(left_column, text="Zalogowano jako:")
-    login_value_label.pack(anchor=tkk.W, padx=(20, 10), pady=(5, 5))
+    login_value_label = ttk.Label(left_column, text="Zalogowano jako:")
+    login_value_label.pack(anchor=ttk.W, padx=(20, 10), pady=(5, 5))
 
-    login_value = tkk.Label(left_column, text=f"{username}")
-    login_value.pack(anchor=tkk.W, padx=(20, 10))
+    login_value = ttk.Label(left_column, text=f"{username}")
+    login_value.pack(anchor=ttk.W, padx=(20, 10), pady=(0, 100))
 
-    balance_label = tkk.Label(left_column, text="Aktualny balans konta:")
-    balance_label.pack(anchor=tkk.W, padx=(20, 10), pady=(20, 10))
+    add_privileges_button = ttk.Button(left_column, text="Nadaj uprawnienia administratora", width=30, bootstyle=(SUCCESS, OUTLINE), command=lambda: show_add_privilege_window(root))
+    add_privileges_button.pack(anchor=ttk.W, padx=(20, 10), pady=(0, 25))
 
-    balance_value = tkk.Label(left_column, text="", font=("Arial", 22))
-    balance_value.pack(anchor=tkk.W, padx=(20, 10), pady=(20, 10))
+    del_privileges_button = ttk.Button(left_column, text="Odbierz uprawnienia administratora", width=30, bootstyle=(DANGER, OUTLINE), command=lambda: show_del_privilege_window(root, username))
+    del_privileges_button.pack(anchor=ttk.W, padx=(20, 10))
 
-    # Pobieranie aktualnego salda z bazy danych
-    balance = database.mysql_select(table="user", column="user_balance", conditions=f"where user_login = '{username}';")
-    balance_value.config(text=f"${balance[0]}")
+    logout_button = ttk.Button(root, text="Wyloguj", width=10, bootstyle=(INFO, OUTLINE), command=logout)
+    logout_button.pack(side=ttk.BOTTOM, padx=(750, 0), pady=(0, 20))
 
-    logout_button = tkk.Button(root, text="Wyloguj", width=10, bootstyle=(INFO, OUTLINE), command=logout)
-    logout_button.pack(side=tkk.BOTTOM, padx=(750, 0), pady=(0, 20))
+    middle_column = ttk.Frame(root)
+    middle_column.pack(side=ttk.LEFT, padx=50)
 
-    middle_column = tkk.Frame(root)
-    middle_column.pack(side=tkk.LEFT, padx=50)
-
-    zaklady_label = tkk.Label(middle_column, text="Lista zakładów:")
-    zaklady_label.pack(anchor=tkk.W, pady=(0, 10))
+    zaklady_label = ttk.Label(middle_column, text="Lista zakładów:")
+    zaklady_label.pack(anchor=ttk.W, pady=(0, 10))
 
     # Dodanie przewijania do listy zakładów
-    zaklady_frame = tkk.Frame(middle_column, relief=tkk.SOLID)
-    zaklady_frame.pack(fill=tkk.BOTH, expand=True)
+    zaklady_frame = ttk.Frame(middle_column, relief=ttk.SOLID)
+    zaklady_frame.pack(fill=ttk.BOTH, expand=1)
 
-    canvas = tkk.Canvas(zaklady_frame)
-    canvas.pack(side=tkk.LEFT, fill=tkk.BOTH, expand=True)
+    canvas = ttk.Canvas(zaklady_frame)
+    canvas.pack(side=ttk.LEFT, fill=ttk.BOTH, expand=1)
 
-    scrollbar = tkk.Scrollbar(zaklady_frame, orient=tkk.VERTICAL, command=canvas.yview)
-    scrollbar.pack(side=tkk.RIGHT, fill=tkk.Y)
+    scrollbar = ttk.Scrollbar(zaklady_frame, orient=ttk.VERTICAL, command=canvas.yview)
+    scrollbar.pack(side=ttk.RIGHT, fill=ttk.Y)
 
     canvas.configure(yscrollcommand=scrollbar.set)
     canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
 
-    zaklady_inner_frame = tkk.Frame(canvas)
-    canvas.create_window((0, 0), window=zaklady_inner_frame, anchor=tkk.NW)
+    zaklady_inner_frame = ttk.Frame(canvas)
+    canvas.create_window((0, 0), window=zaklady_inner_frame, anchor=ttk.NW)
 
     zaklady_inner_frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
 
@@ -192,33 +186,33 @@ def show_admin_panel(username):
     for zaklad in zaklady:
         game_name, bet_result, bet_value, bet_status, bet_date = zaklad
         card = BetCard(zaklady_inner_frame, game_name, bet_value, bet_result, bet_status, bet_date)
-        card.pack(pady=20, fill="x")
+        card.pack(pady=20, fill="x", expand=1)
 
     canvas.update_idletasks()
 
     canvas.configure(scrollregion=canvas.bbox("all"))
 
-    right_column = tkk.Frame(root)
-    right_column.pack(side=tkk.LEFT)
+    right_column = ttk.Frame(root)
+    right_column.pack(side=ttk.LEFT)
 
-    mecze_label = tkk.Label(right_column, text="Lista meczy:")
-    mecze_label.pack(anchor=tkk.W, pady=(0, 10))
+    mecze_label = ttk.Label(right_column, text="Lista meczy:")
+    mecze_label.pack(anchor=ttk.W, pady=(0, 10))
 
     # Dodanie przewijania do listy meczów
-    mecze_frame = tkk.Frame(right_column, relief=tkk.SOLID)
-    mecze_frame.pack(fill=tkk.BOTH, expand=True)
+    mecze_frame = ttk.Frame(right_column, relief=ttk.SOLID)
+    mecze_frame.pack(fill=ttk.BOTH, expand=True)
 
-    mecze_canvas = tkk.Canvas(mecze_frame)
-    mecze_canvas.pack(side=tkk.LEFT, fill=tkk.BOTH, expand=True)
+    mecze_canvas = ttk.Canvas(mecze_frame)
+    mecze_canvas.pack(side=ttk.LEFT, fill=ttk.BOTH, expand=True)
 
-    mecze_scrollbar = tkk.Scrollbar(mecze_frame, orient=tkk.VERTICAL, command=mecze_canvas.yview)
-    mecze_scrollbar.pack(side=tkk.RIGHT, fill=tkk.Y)
+    mecze_scrollbar = ttk.Scrollbar(mecze_frame, orient=ttk.VERTICAL, command=mecze_canvas.yview)
+    mecze_scrollbar.pack(side=ttk.RIGHT, fill=ttk.Y)
 
     mecze_canvas.configure(yscrollcommand=mecze_scrollbar.set)
     mecze_canvas.bind("<Configure>", lambda e: mecze_canvas.configure(scrollregion=mecze_canvas.bbox("all")))
 
-    mecze_inner_frame = tkk.Frame(mecze_canvas)
-    mecze_canvas.create_window((0, 0), window=mecze_inner_frame, anchor=tkk.NW)
+    mecze_inner_frame = ttk.Frame(mecze_canvas)
+    mecze_canvas.create_window((0, 0), window=mecze_inner_frame, anchor=ttk.NW)
 
     mecze_inner_frame.bind("<Configure>", lambda e: mecze_canvas.configure(scrollregion=mecze_canvas.bbox("all")))
 
@@ -239,7 +233,7 @@ def show_admin_panel(username):
     for mecz in mecze:
         match_name, team_1, team_2, country, city, discipline, date = mecz
         card = MatchCard(mecze_inner_frame, match_name, team_1, team_2, country, city, discipline, date)
-        card.pack(pady=20, fill=tkk.X)
+        card.pack(pady=20, fill=ttk.X)
 
     mecze_canvas.update_idletasks()
     mecze_canvas.configure(scrollregion=mecze_canvas.bbox("all"))
@@ -249,52 +243,52 @@ def show_admin_panel(username):
 def show_user_panel(username):
     clear_window()
 
-    user_label = tkk.Label(root, text="Panel użytkkownika", font=("Arial", 16))
+    user_label = ttk.Label(root, text="Panel użytkkownika", font=("Arial", 16))
     user_label.pack(pady=10)
 
-    left_column = tkk.Frame(root)
-    left_column.pack(side=tkk.LEFT, padx=50)
+    left_column = ttk.Frame(root)
+    left_column.pack(side=ttk.LEFT, padx=50)
 
-    login_value_label = tkk.Label(left_column, text="Zalogowano jako:")
-    login_value_label.pack(anchor=tkk.W, padx=(20, 10), pady=(5, 5))
+    login_value_label = ttk.Label(left_column, text="Zalogowano jako:")
+    login_value_label.pack(anchor=ttk.W, padx=(20, 10), pady=(5, 5))
 
-    login_value = tkk.Label(left_column, text=f"{username}")
-    login_value.pack(anchor=tkk.W, padx=(20, 10))
+    login_value = ttk.Label(left_column, text=f"{username}")
+    login_value.pack(anchor=ttk.W, padx=(20, 10))
 
-    balance_label = tkk.Label(left_column, text="Aktualny balans konta:")
-    balance_label.pack(anchor=tkk.W, padx=(20, 10), pady=(20, 10))
+    balance_label = ttk.Label(left_column, text="Aktualny balans konta:")
+    balance_label.pack(anchor=ttk.W, padx=(20, 10), pady=(20, 10))
 
-    balance_value = tkk.Label(left_column, text="", font=("Arial", 22))
-    balance_value.pack(anchor=tkk.W, padx=(20, 10), pady=(20, 10))
+    balance_value = ttk.Label(left_column, text="", font=("Arial", 22))
+    balance_value.pack(anchor=ttk.W, padx=(20, 10), pady=(20, 10))
 
     # Pobieranie aktualnego salda z bazy danych
     balance = database.mysql_select(table="user", column="user_balance", conditions=f"where user_login = '{username}';")
     balance_value.config(text=f"${balance[0]}")
 
-    logout_button = tkk.Button(root, text="Wyloguj", width=10,  bootstyle=(INFO, OUTLINE), command=logout)
-    logout_button.pack(side=tkk.BOTTOM, padx=(750, 0), pady=(0, 20))
+    logout_button = ttk.Button(root, text="Wyloguj", width=10,  bootstyle=(INFO, OUTLINE), command=logout)
+    logout_button.pack(side=ttk.BOTTOM, padx=(750, 0), pady=(0, 20))
 
-    middle_column = tkk.Frame(root)
-    middle_column.pack(side=tkk.LEFT, padx=50)
+    middle_column = ttk.Frame(root)
+    middle_column.pack(side=ttk.LEFT, padx=50)
 
-    zaklady_label = tkk.Label(middle_column, text="Twoje zakłady:")
-    zaklady_label.pack(anchor=tkk.W, pady=(0, 10))
+    zaklady_label = ttk.Label(middle_column, text="Twoje zakłady:")
+    zaklady_label.pack(anchor=ttk.W, pady=(0, 10))
 
     # Dodanie przewijania do listy zakładów
-    zaklady_frame = tkk.Frame(middle_column, relief=tkk.SOLID)
-    zaklady_frame.pack(fill=tkk.BOTH, expand=True)
+    zaklady_frame = ttk.Frame(middle_column, relief=ttk.SOLID)
+    zaklady_frame.pack(fill=ttk.BOTH, expand=True)
 
-    canvas = tkk.Canvas(zaklady_frame)
-    canvas.pack(side=tkk.LEFT, fill=tkk.BOTH, expand=True)
+    canvas = ttk.Canvas(zaklady_frame)
+    canvas.pack(side=ttk.LEFT, fill=ttk.BOTH, expand=True)
 
-    scrollbar = tkk.Scrollbar(zaklady_frame, orient=tkk.VERTICAL, command=canvas.yview)
-    scrollbar.pack(side=tkk.RIGHT, fill=tkk.Y)
+    scrollbar = ttk.Scrollbar(zaklady_frame, orient=ttk.VERTICAL, command=canvas.yview)
+    scrollbar.pack(side=ttk.RIGHT, fill=ttk.Y)
 
     canvas.configure(yscrollcommand=scrollbar.set)
     canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
 
-    zaklady_inner_frame = tkk.Frame(canvas)
-    canvas.create_window((0, 0), window=zaklady_inner_frame, anchor=tkk.NW)
+    zaklady_inner_frame = ttk.Frame(canvas)
+    canvas.create_window((0, 0), window=zaklady_inner_frame, anchor=ttk.NW)
 
     zaklady_inner_frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
 
@@ -314,33 +308,33 @@ def show_user_panel(username):
     for zaklad in zaklady:
         game_name, bet_result, bet_value, bet_status, bet_date = zaklad
         card = BetCard(zaklady_inner_frame, game_name, bet_value, bet_result, bet_status, bet_date)
-        card.pack(pady=5, fill=tkk.X)
+        card.pack(pady=5, fill=ttk.X)
 
     canvas.update_idletasks()
 
     canvas.configure(scrollregion=canvas.bbox("all"))
 
-    right_column = tkk.Frame(root)
-    right_column.pack(side=tkk.LEFT)
+    right_column = ttk.Frame(root)
+    right_column.pack(side=ttk.LEFT)
 
-    mecze_label = tkk.Label(right_column, text="Dostępne mecze do obstawienia:")
-    mecze_label.pack(anchor=tkk.W, pady=(0, 10))
+    mecze_label = ttk.Label(right_column, text="Dostępne mecze do obstawienia:")
+    mecze_label.pack(anchor=ttk.W, pady=(0, 10))
 
     # Dodanie przewijania do listy meczów
-    mecze_frame = tkk.Frame(right_column, relief=tkk.SOLID)
-    mecze_frame.pack(fill=tkk.BOTH, expand=True)
+    mecze_frame = ttk.Frame(right_column, relief=ttk.SOLID)
+    mecze_frame.pack(fill=ttk.BOTH, expand=True)
 
-    mecze_canvas = tkk.Canvas(mecze_frame)
-    mecze_canvas.pack(side=tkk.LEFT, fill=tkk.BOTH, expand=True)
+    mecze_canvas = ttk.Canvas(mecze_frame)
+    mecze_canvas.pack(side=ttk.LEFT, fill=ttk.BOTH, expand=True)
 
-    mecze_scrollbar = tkk.Scrollbar(mecze_frame, orient=tkk.VERTICAL, command=mecze_canvas.yview)
-    mecze_scrollbar.pack(side=tkk.RIGHT, fill=tkk.Y)
+    mecze_scrollbar = ttk.Scrollbar(mecze_frame, orient=ttk.VERTICAL, command=mecze_canvas.yview)
+    mecze_scrollbar.pack(side=ttk.RIGHT, fill=ttk.Y)
 
     mecze_canvas.configure(yscrollcommand=mecze_scrollbar.set)
     mecze_canvas.bind("<Configure>", lambda e: mecze_canvas.configure(scrollregion=mecze_canvas.bbox("all")))
 
-    mecze_inner_frame = tkk.Frame(mecze_canvas)
-    mecze_canvas.create_window((0, 0), window=mecze_inner_frame, anchor=tkk.NW)
+    mecze_inner_frame = ttk.Frame(mecze_canvas)
+    mecze_canvas.create_window((0, 0), window=mecze_inner_frame, anchor=ttk.NW)
 
     mecze_inner_frame.bind("<Configure>", lambda e: mecze_canvas.configure(scrollregion=mecze_canvas.bbox("all")))
 
@@ -361,7 +355,7 @@ def show_user_panel(username):
     for mecz in mecze:
         match_name, team_1, team_2, country, city, discipline, date = mecz
         card = MatchCard(mecze_inner_frame, match_name, team_1, team_2, country, city, discipline, date)
-        card.pack(pady=5, fill=tkk.X)
+        card.pack(pady=5, fill=ttk.X)
 
     mecze_canvas.update_idletasks()
     mecze_canvas.configure(scrollregion=mecze_canvas.bbox("all"))
